34c31ad847302d27960d5e2e6d7940b8e07079f9
ROHIT <himalayancoder@zohomail.in>
2026-03-31 21:15:40 +0530

    Create entry point for back end - index.js

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Student Management System!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
